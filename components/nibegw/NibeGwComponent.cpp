d5f465d0d37d9b1dffba20d1556df8add7d44dc4
Tom Matheussen <tommatheussen5@hotmail.com>
2022-10-04 12:53:42 +0200

    no wrapper for debug callback (#3)

--- components/nibegw/NibeGwComponent.cpp
+++ components/nibegw/NibeGwComponent.cpp
@@ -7,7 +7,7 @@ NibeGwComponent::NibeGwComponent(int uart_no, int dir_pin, int rx_pin, int tx_pi
     gw_->setCallback(std::bind(&NibeGwComponent::callback_msg_received, this, std::placeholders::_1, std::placeholders::_2),
                      std::bind(&NibeGwComponent::callback_msg_token_received, this, std::placeholders::_1, std::placeholders::_2));
 #ifdef ENABLE_NIBE_DEBUG
-    gw_->setDebugCallback(std::bind(&NibeGwComponent::callback_debug_wrapper, this, std::placeholders::_1, std::placeholders::_2);
+    gw_->setDebugCallback(std::bind(&NibeGwComponent::callback_debug, this, std::placeholders::_1, std::placeholders::_2));
 #endif
     gw_->setVerboseLevel(1);
 
