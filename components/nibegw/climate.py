d33f3e4af20f6abc0cb4f549ea31a059074c3d4b
Joakim Plate <elupus@ecce.se>
2025-05-28 21:38:39 +0200

    fix: correct for deprecation
    climate.CLIMATE_SCHEMA is deprecated switch to new method.
    
    Fixes #101

--- components/nibegw/climate.py
+++ components/nibegw/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, sensor
-from esphome.const import CONF_ID, CONF_SENSOR
+from esphome.const import CONF_SENSOR
 from . import NibeGwComponent, nibegw_ns
 
 AUTO_LOAD = ["sensor"]
@@ -11,22 +11,19 @@ NibeGwClimate = nibegw_ns.class_("NibeGwClimate", climate.Climate, cg.Component)
 CONF_GATEWAY = "gateway"
 CONF_SYSTEM = "system"
 
-CONFIG_SCHEMA = cv.All(
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(NibeGwClimate),
-            cv.GenerateID(CONF_GATEWAY): cv.use_id(NibeGwComponent),
-            cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
-            cv.Required(CONF_SYSTEM): cv.int_range(min=1, max=4)
-        }
-    ).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (
+    climate.climate_schema(NibeGwClimate)
+    .extend({
+        cv.GenerateID(CONF_GATEWAY): cv.use_id(NibeGwComponent),
+        cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_SYSTEM): cv.int_range(min=1, max=4)
+    })
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
-
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await climate.new_climate(config)
     await cg.register_component(var, config)
-    await climate.register_climate(var, config)
     gw = await cg.get_variable(config[CONF_GATEWAY])
     cg.add(var.set_gw(gw))
     cg.add(var.set_system(config[CONF_SYSTEM]))
