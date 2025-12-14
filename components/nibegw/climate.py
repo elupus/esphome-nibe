00cf13171c7d228e39e826919ad12af483d95611
Joakim Plate <elupus@ecce.se>
2025-12-14 21:06:53 +0100

    chore: add ruff formatting to python

--- components/nibegw/climate.py
+++ components/nibegw/climate.py
@@ -13,13 +13,16 @@ CONF_SYSTEM = "system"
 
 CONFIG_SCHEMA = (
     climate.climate_schema(NibeGwClimate)
-    .extend({
-        cv.GenerateID(CONF_GATEWAY): cv.use_id(NibeGwComponent),
-        cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
-        cv.Required(CONF_SYSTEM): cv.int_range(min=1, max=4)
-    })
+    .extend(
+        {
+            cv.GenerateID(CONF_GATEWAY): cv.use_id(NibeGwComponent),
+            cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
+            cv.Required(CONF_SYSTEM): cv.int_range(min=1, max=4),
+        }
+    )
     .extend(cv.COMPONENT_SCHEMA)
 )
+
 
 async def to_code(config):
     var = await climate.new_climate(config)
