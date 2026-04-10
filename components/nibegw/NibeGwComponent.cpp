4aa8630f2d2f10d1e7d723ff03dc9b26ec4dfab7
Alex Hermann <gaaf@gmx.net>
2026-04-10 19:04:33 +0200

    Use structured bindings consistently for readability

--- components/nibegw/NibeGwComponent.cpp
+++ components/nibegw/NibeGwComponent.cpp
@@ -138,17 +138,19 @@ void NibeGwComponent::setup() {
 
 void NibeGwComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "NibeGw");
-  for (auto &&[address, timeout] : udp_targets_) {
+  for (auto &&[address, last_seen] : udp_targets_) {
     ESP_LOGCONFIG(TAG, " Target: %s", address.str().c_str());
   }
   for (auto &&address : udp_sources_) {
     ESP_LOGCONFIG(TAG, " Source: %s", address.str().c_str());
   }
-  for (auto const &x : requests_sockets_) {
-    ESP_LOGCONFIG(TAG, " Handler %x:%x Port: %d", std::get<0>(x.first), std::get<1>(x.first), x.second.port);
+  for (const auto &[key, value] : requests_sockets_) {
+    const auto &[address, req] = key;
+    ESP_LOGCONFIG(TAG, " Handler %x:%x Port: %d", address, req, value.port);
   }
-  for (auto const &x : message_listeners_) {
-    ESP_LOGCONFIG(TAG, " Listeners %x:%x Count: %zu", std::get<0>(x.first), std::get<1>(x.first), x.second.size());
+  for (const auto &[key, value] : message_listeners_) {
+    const auto &[address, req] = key;
+    ESP_LOGCONFIG(TAG, " Listeners %x:%x Count: %zu", address, req, value.size());
   }
 }
 
@@ -215,7 +217,10 @@ void NibeGwComponent::loop() {
 
   if (!udp_sources_.size()) {
     // Check for timeouts on dynamic targets
-    std::erase_if(udp_targets_, [&](const auto &item) { return item.second > 0 && now - item.second > TARGET_TIMEOUT_MS; });
+    std::erase_if(udp_targets_, [&](const auto& item) {
+      const auto& [address, last_seen] = item;
+      return last_seen > 0 && now - last_seen > TARGET_TIMEOUT_MS;
+    });
   }
 
   // Poll sockets for incoming packets
